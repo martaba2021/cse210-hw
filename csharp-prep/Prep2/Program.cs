d5377cbf6d1e3f659b6b6f46b188280f648b3b9d
marta.rod <rod21062@byui.edu>
2023-01-08 00:26:57 -0500

    Completed Hw Prep2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Please enter your grade percentage (no decimal points): ");
+        string gradeInText = Console.ReadLine();
+        int gradePercentage = int.Parse(gradeInText);
+
+        string letter = "";
+
+        if (gradePercentage >= 90)
+        {
+            letter = "A";
+
+        }
+        else if (gradePercentage >= 80)
+        {
+            letter ="B";
+        }
+        else if (gradePercentage >= 70)
+        {
+            letter ="C";
+        }
+        else if (gradePercentage >= 60)
+        {
+            letter ="D";
+        }
+        else
+        {
+            letter ="F";
+        }
+            Console.WriteLine($"Your grade is {letter}.");
+        
+        if (gradePercentage >= 70)
+        {
+            Console.WriteLine("Congratulations! You have passed the class.");
+        }
+        else
+        {
+            Console.WriteLine("Unfortunately you did not pass, best luck next time!");
+        }
     }
 }
